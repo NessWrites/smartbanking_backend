5f690d2b9403b47ec31f68f7978b5e5e5bd5f89c
NessWrites <np01ai4s230009@islingtoncollege.edu.np>
2025-03-26 13:28:49 +0545

    removed duplicate lines on importing serializers

--- smart_banking/users/serializers.py
+++ smart_banking/users/serializers.py
@@ -1,6 +1,4 @@
 from rest_framework import serializers
-
-from rest_framework import serializers
 from .models import (
      Admin, AccountType, Loans, TransactionType,
     Account, Customers, User, Transactions
